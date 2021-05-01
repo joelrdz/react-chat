f9b63888837e2dde27382c50286f8dea418d3df2
Joel Rdz <joelrdzh@gmail.com>
2021-05-01 13:03:59 -0500

    Implementing authentication (localStorage)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="2f730b9f-05d2-4aea-b0a3-8357bfb064ca"
-      userName="blackwidow"
-      userSecret="123123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
